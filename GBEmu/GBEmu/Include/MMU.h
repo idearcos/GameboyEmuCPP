dc6bc9d31e8cd4876ebe1bfa321bc1b73202f0a3
idearcos <inigodearcos@gmail.com>
2015-09-18 00:34:06 +0900

    Start implementation of MMU
    Added initialization of BIOS

--- GBEmu/GBEmu/Include/MMU.h
+++ GBEmu/GBEmu/Include/MMU.h
@@ -1,12 +1,21 @@
 #pragma once
 
 #include <cstdint>
+#include <array>
 
 class MMU
 {
 public:
+	MMU();
+	~MMU() = default;
+
 	uint8_t Read8bitFromMemory(uint16_t addr) const;
 	uint16_t Read16bitFromMemory(uint16_t addr) const;
 	void Write8bitToMemory(uint16_t addr, uint8_t value);
 	void Write16bitToMemory(uint16_t addr, uint16_t value);
+
+private:
+	const std::array<uint8_t, 256> bios_;
+	bool bios_loaded_{ true };
+
 };
